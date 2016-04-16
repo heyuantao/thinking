7fc67048bf065da998a98b1f8946edc63e000440
he_yuan_tao <he_yuan_tao@163.com>
2016-04-16 18:21:34 +0800

    new django project

---
+++ src/cloudhoststatus/hostinformation/views.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import urls
+
+successStatus={"status":"success"}
+rejectStatus={"status":"reject"}
+
+# Create your views here.
+class IndexPage(APIView):
+    def get(self,request):
+        pathDict={}
+        pathList=[]
+        for item in urls.urlpatterns:
+            print item.regex.pattern
+            pathList.append(item.regex.pattern)
+        pathDict['paths']=pathList
+        return Response(pathDict)
