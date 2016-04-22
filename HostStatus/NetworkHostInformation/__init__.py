dc513bba4435bb77d8b0a5195499d96cc7466d2e
he_yuan_tao <he_yuan_tao@163.com>
2016-04-22 21:33:07 +0800

    change

--- HostStatus/NetworkHostInformation/__init__.py
+++ HostStatus/NetworkHostInformation/__init__.py
@@ -10,18 +10,21 @@ class NetworkHostInformation(object):
     def __init__(self,networkAddress):
         self.networkAddress=networkAddress
         networkObject=IPNetwork(self.networkAddress)
-        if self.networkObject.prefixlen!=24:
-            return False
-        else:
-            return True
+        if networkObject.prefixlen!=24:
+            raise Exception("network address format error !")
         
     def getHostStatus(self):
         nm = nmap.PortScanner()
         nm.scan(hosts=self.networkAddress, arguments='-n -sP -PE')
-        self.hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-        return self.hosts_list
+        hostStatusDic={}
+        #self.hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+        for x in nm.all_hosts():
+            hostStatusDic[x]=nm[x]['status']['state']
+        return hostStatusDic
 
 if __name__=='__main__':
-    networkHostInformation=NetworkHostInformation('192.168.33.0/24')
+    #networkHostInformation=NetworkHostInformation('202.196.166.1/24')
+    #print networkHostInformation.getHostStatus()
+    networkHostInformation=NetworkHostInformation('192.168.130.0/24')
     print networkHostInformation.getHostStatus()
     
