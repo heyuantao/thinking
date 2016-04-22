54168ea2d5de8c84658781368100fa781658d1d6
he_yuan_tao <he_yuan_tao@163.com>
2016-04-22 14:29:17 +0800

    s

--- HostStatus/NetworkHostInformation/__init__.py
+++ HostStatus/NetworkHostInformation/__init__.py
@@ -1,8 +1,20 @@
+# This program is used for host discovery
+#nm = nmap.PortScanner()
+#nm.scan(hosts=self.networkAddress, arguments='-n -sP -PE')
+#self.hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
 import nmap
+from netaddr import IPNetwork
 
 class NetworkHostInformation(object):
     def __init__(self,networkAddress):
         self.networkAddress=networkAddress
+        networkObject=IPNetwork(self.networkAddress)
+        if self.networkObject.prefixlen!=24:
+            return False
+        else:
+            return True
+        
     def getHostStatus(self):
         nm = nmap.PortScanner()
         nm.scan(hosts=self.networkAddress, arguments='-n -sP -PE')
