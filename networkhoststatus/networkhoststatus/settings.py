31e1206bf4ae0f5236acc9e5e4cb32f3f233c15f
he_yuan_tao <he_yuan_tao@163.com>
2016-04-24 18:15:20 +0800

    save

--- networkhoststatus/networkhoststatus/settings.py
+++ networkhoststatus/networkhoststatus/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'networkhoststatus.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 
