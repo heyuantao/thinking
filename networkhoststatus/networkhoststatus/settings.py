f759b1b7d04cfbd2c3f752459541f32db664839c
he_yuan_tao <he_yuan_tao>
2016-05-08 19:29:47 +0800

    s

--- networkhoststatus/networkhoststatus/settings.py
+++ networkhoststatus/networkhoststatus/settings.py
@@ -128,6 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/networkhoststatus/static/'
-MEDIA_URL = '/networkhoststatus/media/'
+STATIC_URL = '/network/static/'
+MEDIA_URL = '/network/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- networkhoststatus/networkhoststatus/urls.py
+++ networkhoststatus/networkhoststatus/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^api/', include('MainApplication.urls')),
+    url(r'^network/', include('MainApplication.urls')),
     #url(r'^admin/', admin.site.urls),
 ]
